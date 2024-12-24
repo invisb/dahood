552f5087e97865b1e045a4666746ba5f2794f9a0
invisb <181142157+invisb@users.noreply.github.com>
2024-12-24 11:09:47 -0600

    Create autoCodeRedeemer.lua

---
+++ autoCodeRedeemer.lua
@@ -0,0 +1,26 @@
+local success, response = pcall(request, {
+    Url = "https://gamerant.com/roblox-da-hood-codes/",
+    Method = "GET",
+})
+
+if success and response.StatusCode == 200 then
+    local html = response.Body
+    local codes = {}
+    
+    local activeSection = string.match(html, "All Active Da Hood Codes(.-)All Expired Da Hood Codes")
+    if activeSection then
+        for code in string.gmatch(activeSection, "<strong>(.-)</strong>") do
+            if not code:find("%(NEW%)") then
+                code = code:gsub("%s+", "")
+                table.insert(codes, code)
+            end
+        end
+        
+        for _, code in ipairs(codes) do
+            local args = {[1] = "EnterPromoCode",[2] = code}
+            game:GetService("ReplicatedStorage"):WaitForChild("MainEvent"):FireServer(unpack(args))
+        end
+    end
+else
+    warn("failed to get website (executor problem)")
+end
